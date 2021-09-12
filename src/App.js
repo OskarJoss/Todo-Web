2a749a94386547b6489a6635f34d36e823910cbb
oskarjoss <oskar.sjolin.joss@gmail.com>
2021-09-12 15:41:11 +0200

    fetch data from back-end

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8080/todos")
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.success) {
+          setTodos(json.data);
+        }
+      });
+  }, []);
+  return (
+    <div className="App">
+      <ol>
+        {todos.map((todo, i) => {
+          return <li key={i}>{todo.title}</li>;
+        })}
+      </ol>
+    </div>
+  );
 }
 
 export default App;
